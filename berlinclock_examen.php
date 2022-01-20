a8e66be737c1688480e9018444bcff86aab43a64
Sacha Norden <sacha.norden@student.vinci.be>
2022-01-20 22:41:38 +0100

    add firts test

--- berlinclock_examen.php
+++ berlinclock_examen.php
@@ -2,12 +2,34 @@
 
 class berlinclock_examen
 {
-    private DateTime $dateTime;
-    private int $seconde;
-    private int $fiveMinutes;
-    private int $minutes;
-    private int $hours;
-    private int $fiveHours;
 
-    public function __con
+    private $password;
+
+    public function __construct()
+    {
+        $this->password=$this->password;
+    }
+    public function minCharacters($string)
+    {
+        return(strlen($string)<8);
+    }
+
+    public function majLetter($string)
+    {
+        return (preg_match('/[A-Z]/',$string));
+    }
+    public function minLetter($string)
+    {
+        return (preg_match('/[a-z]/',$string));
+    }
+
+    public function containNum($string)
+    {
+        return (preg_match('~[0-9]+~',$string));
+    }
+    public function specialCharacters($string)
+    {
+        return (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/',$string));
+    }
+
 }
--- berlinclock_examen_test.php
+++ berlinclock_examen_test.php
@@ -1,6 +1,15 @@
 <?php
+use berlinclock_examen;
+use PHPUnit\Framework\TestCase;
 
-class berlinclock_examen_test
+class berlinclock_examen_test extends TestCase
 {
+    public function testpassword(){
 
+        $passowrd1= new berlinclock_examen("abc123?");
+
+        $actual1=$passowrd1->containNum($passowrd1);
+
+        $this->assertEquals(false,$passowrd1);
+    }
 }
