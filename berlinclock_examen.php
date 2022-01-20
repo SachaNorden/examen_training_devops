da776f4b9f43ccd7b45b8a4388d67b927cefbd23
Sacha Norden <sacha.norden@student.vinci.be>
2022-01-20 19:33:24 +0100

    add test

--- berlinclock_examen.php
+++ berlinclock_examen.php
@@ -2,5 +2,12 @@
 
 class berlinclock_examen
 {
+    private DateTime $dateTime;
+    private int $seconde;
+    private int $fiveMinutes;
+    private int $minutes;
+    private int $hours;
+    private int $fiveHours;
 
+    public function __con
 }
---
+++ berlinclock_examen_test.php
@@ -0,0 +1,6 @@
+<?php
+
+class berlinclock_examen_test
+{
+
+}
